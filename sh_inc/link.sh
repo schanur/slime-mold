083ee2c421c95ce1759ec8417916a68eef0b0751
Bjoern Griebenow <b.griebenow@web.de>
2017-06-10 17:49:23 +0200

    Add unfinished link module to connect 2 switches

---
+++ sh_inc/link.sh
@@ -0,0 +1,79 @@
+# Connect 2 switches.
+function link__add
+{
+
+    local VDE_SWITCH_NAME
+    local SWITCH_RUNNING
+
+    VDE_SWITCH_NAME=$1
+
+    SWITCH_RUNNING=$(switch__status ${VDE_SWITCH_NAME})
+    if [ "${SWITCH_RUNNING}" = "Online" ]; then
+        echo "Switch ${VDE_SWITCH_NAME} is already running."
+        exit 1
+    fi
+    vde_switch --daemon --sock /tmp/${PROGRAM_SHORT_NAME}__switch__${VDE_SWITCH_NAME} --mgmt /tmp/${PROGRAM_SHORT_NAME}__switch__${VDE_SWITCH_NAME}.mgmt
+    # TODO: Find a way to sync only the files we have created.
+    sync
+}
+
+# Pull the link between the two components. A component can  be everything.
+function link__remove
+{
+    local VDE_SWITCH_NAME
+    local SWITCH_RUNNING
+    local SWITCH_PS_LINE
+    local PID
+
+    VDE_SWITCH_NAME=${1}
+
+    SWITCH_RUNNING=$(switch__status ${VDE_SWITCH_NAME})
+    if [ "${SWITCH_RUNNING}" = "Offline" ]; then
+        echo "Switch ${VDE_SWITCH_NAME} is not running."
+        exit 1
+    fi
+
+    SWITCH_PS_LINE=$(ps aux |grep "vde" |grep "/tmp/${PROGRAM_SHORT_NAME}__switch__${VDE_SWITCH_NAME}")
+    PID=$(echo ${SWITCH_PS_LINE} |sed 's/\ \ /\ /g' |sed 's/\ \ /\ /g' |sed 's/\ \ /\ /g' |cut -d " " -f 2)
+    kill ${PID}
+    # TODO: Find a way to sync only the files we have created.
+    sync
+}
+
+# Login into the terminal of the switch.
+function switch__console
+{
+    local VDE_SWITCH_NAME
+
+    VDE_SWITCH_NAME=${1}
+    unixterm /tmp/${PROGRAM_SHORT_NAME}__switch__${VDE_SWITCH_NAME}.mgmt
+}
+
+# Prints status string "Online" or "Offline"
+function switch__status
+{
+    local STATUS_STR="Online"
+    local PID_LINE_CNT=$(ps aux | grep "vde_switch" | grep -c "/tmp/${PROGRAM_SHORT_NAME}__switch__${VDE_SWITCH_NAME}")
+    if [ "${PID_LINE_CNT}" != "1" ]; then
+        echo "Offline"
+    fi
+
+    echo ${STATUS_STR}
+}
+
+# Print a list of all running switches.
+function switch__list
+{
+    local VDE_SWITCH_NAME
+
+    for VDE_SWITCH_NAME in $(find /tmp -maxdepth 1 -name "${PROGRAM_SHORT_NAME}__switch__*.mgmt"); do
+        echo ${VDE_SWITCH_NAME} | sed "s|/tmp/${PROGRAM_SHORT_NAME}__switch__||g" | sed 's|\.mgmt||g'
+    done
+}
+
+# Connect 2 VDE switch instances.
+function switch__connect_to_switch
+{
+    echo "Not implemented yet"
+    exit 1
+}
