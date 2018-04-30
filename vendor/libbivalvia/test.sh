97fb181af1ae98bf933724aa3a64ffe9afedc832
Bjoern Griebenow <b.griebenow@web.de>
2018-05-01 00:45:37 +0200

    Fix test harness script

--- vendor/libbivalvia/test.sh
+++ vendor/libbivalvia/test.sh
@@ -4,7 +4,7 @@ BIVALVIA_PATH="   $(dirname "${BASH_SOURCE[0]}")/bivalvia"
 MODULE_TEST_PATH="$(dirname "${BASH_SOURCE[0]}")/test/module"
 
 
-for MODULE_TEST_SCRIPT in $(find "${MODULE_TEST_PATH}" "${MODULE_TEST_PATH}" -mindepth 1 -maxdepth 1); do
+for MODULE_TEST_SCRIPT in $(find "${MODULE_TEST_PATH}" -mindepth 1 -maxdepth 1); do
     echo "${MODULE_TEST_SCRIPT}"
     "${MODULE_TEST_SCRIPT}"
 done
